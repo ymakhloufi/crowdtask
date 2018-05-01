01a50280a0a982f4947b8dfc6851d4a5f4ab4392
Yassin Makhloufi <yassin@studi.se>
2018-05-01 19:12:57 +0200

    wip registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,71 +2,64 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Yama\User\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Yama\User\User;
+use Yama\User\UserRepository;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private   $userRepository;
+    protected $redirectTo = '/';
+
+
+    public function __construct(UserRepository $userRepository)
     {
         $this->middleware('guest');
+        $this->userRepository = $userRepository;
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
+
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name'     => 'required|string|max:32',
+            'email'    => 'required|string|email|max:128|unique:users',
+            'gender'   => 'required|in:male,female',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
 
+
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
+     *
      * @return \Yama\User\User
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+        User::unguard();
+        $user = $this->userRepository->create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
+            'password' => bcrypt($data['password']),
+            'gender'   => $data['gender'],
         ]);
+        User::reguard();
+
+        Mail::send('mail.welcome', ['user' => $user], function (Message $m) use ($user) {
+            //$m->from(config('mail.from.address'), config('mail.from.name'));
+            $m->to($user->email, $user->name)->subject('Welcome to ' . config('app.name') . "!");
+        });
+
+        return $user;
     }
 }
