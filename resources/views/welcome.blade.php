db3de7b2ef5988d69cf3dc39a2969495e81c796d
Yassin Makhloufi <yassin@studi.se>
2018-05-07 09:45:39 +0200

    WIP theming

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,20 @@
                         @guest
                             You must first login!
                         @else
-                            You are logged in!
+                            <p>You are logged in!</p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam euismod quam quis erat
+                                viverra, sit amet finibus nisi porttitor. Sed non libero purus. Nullam consectetur et
+                                sem eget malesuada. Sed quis vehicula nibh. Sed elementum turpis sit amet leo rhoncus,
+                                eget ornare elit tincidunt. Fusce tristique leo sit amet erat laoreet feugiat. Mauris
+                                hendrerit nulla ligula, in consequat justo suscipit sodales.</p>
+
+                            <p>Vivamus dapibus turpis sed rhoncus interdum. Morbi volutpat quis massa a sollicitudin.
+                                Duis vitae consectetur erat. Etiam nec orci dignissim, feugiat leo non, suscipit nunc.
+                                Nulla massa risus, luctus ac feugiat et, maximus at mi. Curabitur dapibus ante venenatis
+                                viverra consectetur. Fusce vulputate ultricies elementum. Nullam varius ullamcorper
+                                neque a faucibus. Etiam pretium pulvinar mattis. Aenean mollis erat a tortor cursus,
+                                lobortis semper turpis consequat.</p>
                         @endguest
 
                     </div>
