0edd6669fda7766b632fb1c08bd3fef19b095f9b
Yassin Makhloufi <yassin@studi.se>
2018-05-16 16:53:04 +0200

    update Login and Sign-Up views

--- resources/views/dashboardGuests.blade.php
+++ resources/views/dashboardGuests.blade.php
@@ -5,8 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-12 guest-welcome-box">
                 <div class="row">
-                    @include('auth.login-form')
-                    @include('auth.signup-form')
+                    <div class="col-md-6 login-left">
+                        <h5 class="mb-0" style="text-align: center">I Have An Account</h5>
+                        @include('auth.login-form')
+                    </div>
+                    <div class="col-md-6">
+                        <h5 style="text-align: center">I Am New Here</h5>
+                        @include('auth.signup-form')
+                    </div>
                 </div>
             </div>
         </div>
