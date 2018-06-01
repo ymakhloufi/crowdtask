678762e2f82549cbdc0258032e1b0bb8591cd967
Yassin Makhloufi <yassin@studi.se>
2018-06-01 09:13:45 +0200

    add badge-template and improve rating style and template

---
+++ resources/views/partials/_badge.blade.php
@@ -0,0 +1,6 @@
+<img class="ct-badge"
+     src="{{$badge->imageUrl}}"
+     data-toggle="tooltip"
+     data-html="true"
+     title="<b>{{$badge->title}}</b><br/>{{$badge->description}}"
+/>
--- resources/views/partials/_rating.blade.php
+++ resources/views/partials/_rating.blade.php
@@ -1,3 +1,9 @@
-<div class="star-ratings-sprite" title="Rating: {{$rating}}/5 hearts" data-toggle="tooltip" style="margin-top: -10px;">
-    <span class="star-ratings-sprite-rating" style="width:{{$rating*100/5}}%"></span>
+<div class="star-ratings-sprite"
+     title="@if($rating) Rating: {{round($rating, 1)}}/5 hearts @else not rated yet @endif"
+     data-toggle="tooltip"
+     style="margin-top: -10px;"
+>
+    <span class="star-ratings-sprite-rating"
+          style="width:{{($rating ?: 0)*100/5}}%">
+    </span>
 </div>
