bb8aefaefc525d7ad5d31e9a4572b1236c301821
Yassin Makhloufi <yassin@studi.se>
2018-06-01 09:10:24 +0200

    add pseudo-cached user statistics and password-reset funcion

--- app/Yama/User/User.php
+++ app/Yama/User/User.php
@@ -46,6 +46,19 @@ class User extends \Illuminate\Foundation\Auth\User
     /** @var null|Collection $badges */
     private $badges = null;
 
+    /** @var null|array */
+    private $statistics = null;
+
+
+    public function setPassword(string $password, bool $saving = true)
+    {
+        $this->password = bcrypt($password);
+
+        if ($saving) {
+            $this->save();
+        }
+    }
+
 
     public function tags(): BelongsToMany
     {
@@ -94,5 +107,47 @@ class User extends \Illuminate\Foundation\Auth\User
         }
 
         return $this->badges;
+    }
+
+
+    public function getStatistics(): array
+    {
+        // Using the collections here was significantly faster than the query-builder.
+        // I assume because we avoid the repeated overhead of talking to the database.
+        if ($this->statistics === null) {   // cache the result for using this during the same request
+            $this->statistics = [
+                'authoredTasks'       => [
+                    'approved' => $this->authoredTasks->where('approved_at', '!==', null)->count(),
+                    'pending'  => $this->authoredTasks->where('approved_at', '===', null)->count(),
+                    'all'      => $this->authoredTasks->count(),
+                ],
+                'issuedAssignments'   => [
+                    'new'       => $this->issuedAssignments->where('status', 'new')->count(),
+                    'accepted'  => $this->issuedAssignments->where('status', 'accepted')->count(),
+                    'rejected'  => $this->issuedAssignments->where('status', 'rejected')->count(),
+                    'fulfilled' => $this->issuedAssignments->where('status', 'fulfilled')->count(),
+                    'passed'    => $this->issuedAssignments->where('status', 'passed')->count(),
+                    'failed'    => $this->issuedAssignments->where('status', 'failed')->count(),
+                    'all'       => $this->issuedAssignments->count(),
+                ],
+                'receivedAssignments' => [
+                    'new'       => $this->receivedAssignments->where('status', 'new')->count(),
+                    'accepted'  => $this->receivedAssignments->where('status', 'accepted')->count(),
+                    'rejected'  => $this->receivedAssignments->where('status', 'rejected')->count(),
+                    'fulfilled' => $this->receivedAssignments->where('status', 'fulfilled')->count(),
+                    'passed'    => $this->receivedAssignments->where('status', 'passed')->count(),
+                    'failed'    => $this->receivedAssignments->where('status', 'failed')->count(),
+                    'all'       => $this->receivedAssignments->count(),
+                ],
+                'writtenComments'     => [
+                    'tasks'       => $this->writtenComments->where('commentable_type', Task::class)->count(),
+                    'assignments' => $this->writtenComments->where('commentable_type', Assignment::class)->count(),
+                    'comments'    => $this->writtenComments->where('commentable_type', Comment::class)->count(),
+                    'all'         => $this->writtenComments->count(),
+                ],
+            ];
+        }
+
+        return $this->statistics;
     }
 }
